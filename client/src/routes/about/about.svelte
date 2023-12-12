2adc9e2577914857f25167039a9ce6577f6a8121
Jon Bernal <jbernalwrc@gmail.com>
2023-12-11 16:49:50 -0800

    WIP: swapping comps.

---
+++ client/src/routes/about/about.svelte
@@ -0,0 +1,6 @@
+<script></script>
+<h1>About Me</h1>
+
+<img src="../../../static/Portfolio-headshot-pencil-small.png" alt="Pencil style portrait of Jon"/>
+
+<style></style>
---
+++ client/useful-info/color-palette.md
@@ -0,0 +1,5 @@
+Primary orange: #e95420
+Secondary Blue: #0089DF
+Dark Gray: #262626
+Light Gray: #333333
+White: #fafafa
