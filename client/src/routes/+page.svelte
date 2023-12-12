43dc60f2f9cc0e1cd21f74cd901bf17fd00c8471
jon Bernal <jbernalwrc@gmail.com>
2023-12-11 19:01:31 -0800

    add simple content to test routing.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Whidbey Island Web Dev</h1>
+<a href="/">Home</a>
+<a href="/about">About Me</a>
+<a href="/portfolio">Portfolio</a>
+<a href="/blog">Blog</a>
--- client/src/routes/about/about.svelte
+++ client/src/routes/about/about.svelte
@@ -1,6 +1,8 @@
 <script></script>
 <h1>About Me</h1>
+<a href="/">Home</a>
 
 <img src="../../../static/Portfolio-headshot-pencil-small.png" alt="Pencil style portrait of Jon"/>
+
 
 <style></style>
