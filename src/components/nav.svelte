5a519af24e7408ca879050eacdc627c679218443
jon Bernal <jbernalwrc@gmail.com>
2023-12-14 15:13:33 -0800

    fix sidebar

---
+++ src/components/nav.svelte
@@ -0,0 +1,12 @@
+<script></script>
+  <slot name="nav">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/portfolio">portfolio</a>
+    <a href="/contact">contact me</a>
+  </slot>
+<style>
+  * {
+    border: 1px solid red;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,34 @@
   import "../app.pcss";
   import '../globalStyles.css'
   import { AppShell } from '@skeletonlabs/skeleton';
+	import "../components/nav.svelte"
+	import type { AfterNavigate } from '@sveltejs/kit';
+	import { afterNavigate } from '$app/navigation';
+
+	afterNavigate((params: AfterNavigate) => {
+			const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
+			const elemPage = document.querySelector('#page');
+			if (isNewPage && elemPage !== null) {
+					elemPage.scrollTop = 0;
+			}
+	});
 </script>
 
-<AppShell>
+
+
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
 	<svelte:fragment slot="header">Header</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Hidden below Tailwind's large breakpoint -->
+		<div id="sidebar-left" class="hidden lg:block">Sidebar</div>
+	</svelte:fragment>
+
 	<slot />
-	<!-- ---- / ---- -->
 	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-	<!-- (footer) -->
 </AppShell>
 
 <style>
-  
+ * {
+	border: 1px solid red;
+ } 
 </style>
