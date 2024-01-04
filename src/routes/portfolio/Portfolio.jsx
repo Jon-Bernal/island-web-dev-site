d721c47f8ec2bd88882be64185cec85d73748382
Jon Bernal <jbernalwrc@gmail.com>
2024-01-03 16:30:39 -0800

    WIP: add cards to portfolio. Still needs to be contained to show multiple on a line.

--- src/routes/portfolio/Portfolio.jsx
+++ src/routes/portfolio/Portfolio.jsx
@@ -1,3 +1,48 @@
+import { Card, Image, Group, Text, Button } from "@mantine/core";
+
+const sites = [
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+];
+
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      {sites.map((site) => (
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+          <Card.Section>
+            <Image src={site.image.src} height={160} alt={site.image.alt} />
+          </Card.Section>
+
+          <Group justify="space-between" mt="md" mb="xs">
+            <Text fw={500}>{site.title}</Text>
+          </Group>
+
+          <Text size="sm" c="dimmed">
+            {site.description}
+          </Text>
+
+          <Button color="blue" mt="md" radius="md">
+            View
+          </Button>
+        </Card>
+      ))}
+    </div>
+  );
 }
