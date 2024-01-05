5a9e9ba93d3fd9788640da344e7cbe6698139b72
jon Bernal <jbernalwrc@gmail.com>
2024-01-05 09:32:36 -0800

    add: portfolio assets, portfolio card responsiveness.

--- src/routes/portfolio/Portfolio.jsx
+++ src/routes/portfolio/Portfolio.jsx
@@ -1,6 +1,96 @@
-import { Card, Image, Group, Text, Button } from "@mantine/core";
+import { Card, Image, Group, Text, Button, Grid } from "@mantine/core";
 
 const sites = [
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    image: {
+      src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+      alt: "Norway",
+    },
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
   {
     image: {
       src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
@@ -23,26 +113,28 @@ const sites = [
 
 export default function Portfolio() {
   return (
-    <div>
+    <Grid>
       {sites.map((site) => (
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image src={site.image.src} height={160} alt={site.image.alt} />
-          </Card.Section>
+        <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4, lg: 3, xl: 2 }}>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image src={site.image.src} height={160} alt={site.image.alt} />
+            </Card.Section>
 
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>{site.title}</Text>
-          </Group>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>{site.title}</Text>
+            </Group>
 
-          <Text size="sm" c="dimmed">
-            {site.description}
-          </Text>
+            <Text size="sm" c="dimmed">
+              {site.description}
+            </Text>
 
-          <Button color="blue" mt="md" radius="md">
-            View
-          </Button>
-        </Card>
+            <Button color="blue" mt="md" radius="md">
+              View
+            </Button>
+          </Card>
+        </Grid.Col>
       ))}
-    </div>
+    </Grid>
   );
 }
