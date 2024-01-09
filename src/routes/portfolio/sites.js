fc4d1db0de7ad44d1c85b40c9da6a539109a23f4
jon Bernal <jbernalwrc@gmail.com>
2024-01-08 19:10:02 -0800

    wip: portfolio content and about content.

---
+++ src/routes/portfolio/sites.js
@@ -0,0 +1,35 @@
+export const sites = [
+  {
+    images: [
+      {
+        src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+        alt: "Norway",
+      },
+    ],
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    images: [
+      {
+        src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+        alt: "Norway",
+      },
+    ],
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+  {
+    images: [
+      {
+        src: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png",
+        alt: "Norway",
+      },
+    ],
+    title: "Norway Fjord Adventures",
+    description:
+      "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+  },
+];
