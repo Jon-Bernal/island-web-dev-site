37d763771857d550ddfef78234acdd94f114abab
Jon Bernal <jbernalwrc@gmail.com>
2024-02-24 11:23:54 -0800

    WIP: styling the contact page.

--- src/routes/contact/Contact.jsx
+++ src/routes/contact/Contact.jsx
@@ -1,9 +1,19 @@
-// import styles from "./contact.module.css";
+import styles from "./contact.module.css";
 
 import { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Button,
+  TextInput,
+  TextArea,
+} from "grommet";
 
 export default function Contact() {
-  useState({ initialValues: { name: "", email: "", message: "" } });
+  const [msg, setMsg] = useState({
+    initialValues: { name: "", email: "", message: "" },
+  });
   // function validate(name, email, message) {
   //   let isValid = true;
   //   if (name === "") isValid = false;
@@ -13,36 +23,44 @@ export default function Contact() {
   // }
 
   return (
-    <form onSubmit={(values) => console.log(values)}>
-      {/* <Fieldset legend="Message Whidbey Island Web Dev"> */}
-      <input
-        type="text"
-        // size="sm"
-        // radius="sm"
-        // label="Name"
-        // withAsterisk
-        placeholder="John Smith"
-      />
-      <input
-        // size="sm"
-        // radius="sm"
-        // label="Email"
-        // withAsterisk
-        placeholder="example@provider.com"
-        type="email"
-      />
-      <textarea
-        name="message"
-        // label="Message"
-        // withAsterisk
-        // placeholder="Input placeholder"
-        // autodsize
-        // minRows={3}
-      />
-      {/* <Group justify="flex-end" mt="md"> */}
-      <button type="submit">Submit</button>
-      {/* </Group> */}
-      {/* </Fieldset> */}
-    </form>
+    <Card height="medium" width="large" background="dark-2">
+      <CardHeader pad="medium">
+        <h2>Contact Me</h2>
+      </CardHeader>
+      <CardBody pad="medium">
+        <form
+          onSubmit={(values) => console.log(values)}
+          className={styles.contact_form}
+        >
+          <TextInput
+            placeholder="John Smith"
+            value={msg.name}
+            onChange={(event) => setMsg({ ...msg, name: event.target.value })}
+          />
+          <TextInput
+            placeholder="example@provider.com"
+            value={msg.email}
+            className={styles.input}
+            onChange={(event) => setMsg({ ...msg, email: event.target.value })}
+          />
+          <TextArea
+            placeholder="Write your message here."
+            value={msg.message}
+            className={styles.input}
+            onChange={(event) =>
+              setMsg({ ...msg, message: event.target.value })
+            }
+          />
+          <Button
+            primary
+            type="submit"
+            label="Submit"
+            alignSelf="end"
+            color={"#0488ff"}
+            margin={{ top: "20px" }}
+          />
+        </form>
+      </CardBody>
+    </Card>
   );
 }
