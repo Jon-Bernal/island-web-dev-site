e9883f3e3a9ba59e7dfed96a93a1eca359f89ce1
jon Bernal <jbernalwrc@gmail.com>
2024-02-27 23:04:22 -0800

    WIP: started to add full description for each site. Commit to save progress.

--- src/routes/portfolio/Portfolio.jsx
+++ src/routes/portfolio/Portfolio.jsx
@@ -1,48 +1,54 @@
+import { useEffect, useState } from "react";
 import { sites } from "./sites";
 import { Card, CardHeader, CardBody, Image } from "grommet";
 import styles from "./portfolio.module.css";
+
 export default function Portfolio() {
+  const [showSite, setShowSite] = useState(-1);
+
+  useEffect(() => {
+    console.log("showSite :>> ", showSite);
+  }, [showSite]);
+
   return (
     <div className={styles.container}>
-      {sites.map((site) => (
-        <Card
-          height="medium"
-          width="medium"
-          background="dark-2"
-          className={styles.card}
+      {sites.map((site, i) => (
+        <button
+          className={styles.card_button}
+          onClick={() => {
+            if (showSite === i) {
+              setShowSite(-1);
+            } else {
+              setShowSite(i);
+            }
+          }}
         >
-          <CardHeader pad="none">
-            <Image src={site.images[0].src} fit="cover" />
-          </CardHeader>
-          <CardBody pad="small">
-            <h4 className={styles.title}>{site.title}</h4>
-            <p className={styles.shortDesc}>{site.shortDescription}</p>
-          </CardBody>
-        </Card>
-        //     <Grid.Col span={{ base: 12, xs: 6, sm: 6, md: 4, lg: 3, xl: 2 }}>
-        //       <Card shadow="sm" padding="lg" radius="md" withBorder>
-        //         <Card.Section>
-        //           <Image
-        //             src={site.images[0].src}
-        //             height={160}
-        //             alt={site.images[0].alt}
-        //           />
-        //         </Card.Section>
-
-        //         <Group justify="space-between" mt="md" mb="xs">
-        //           <Text fw={500}>{site.title}</Text>
-        //         </Group>
-
-        //         <Text size="sm" c="dimmed">
-        //           {site.shortDescription}
-        //         </Text>
-
-        //         <Button color="blue" mt="md" radius="md">
-        //           View
-        //         </Button>
-        //       </Card>
-        //     </Grid.Col>
+          <Card
+            height="medium"
+            width="medium"
+            background="dark-2"
+            className={styles.card}
+          >
+            <CardHeader pad="none">
+              <Image src={site.images[0].src} fit="cover" />
+            </CardHeader>
+            <CardBody pad="small">
+              <h4 className={styles.title}>{site.title}</h4>
+              <p className={styles.shortDesc}>{site.shortDescription}</p>
+            </CardBody>
+          </Card>
+        </button>
       ))}
+      {showSite !== -1 && (
+        <div className={styles.fullSiteContainer}>
+          <image
+            src={sites[showSite].images[0].src}
+            alt={sites[showSite].images[0].alt}
+          />
+          <h3>{sites[showSite].title}</h3>
+          <p>{sites[showSite].description}</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/routes/portfolio/sites.js
+++ src/routes/portfolio/sites.js
@@ -19,53 +19,12 @@ export const sites = [
   {
     images: [
       {
-        src: guild,
-        alt: "Guild Education Home Page",
-      },
-    ],
-    title: "Guild Education",
-    shortDescription: ``,
-    description: `I built mainly backend services at guild.`,
-  },
-  {
-    images: [
-      {
-        src: lambda,
-        alt: "Lambda Group Organizer Home Page",
-      },
-    ],
-    title: "Lambda Group Organizer",
-    shortDescription: ``,
-    description: `Lambda Group Organizer: Streamlining Collaborations in Education
-
-      In the realm of collaborative projects, chaos reigned during group sign-ups at our school. Unintentional deletions, collisions, and tedious data formatting plagued the process. Enter Lambda Group Organizer, a solution born out of necessity and innovation.
-      
-      🌐 Addressing the Chaos: From Spreadsheet Woes to Seamless Sign-ups
-      
-      Say farewell to the spreadsheet struggles. Lambda Group Organizer revolutionized project sign-ups by empowering teachers and students alike. No more accidental deletions or clashes – this tool transformed the sign-up process into a seamless experience.
-      
-      📊 Teacher Empowerment: Effortless Project Management
-      
-      Teachers gained control with the ability to upload project details directly into the system, specifying student slots and project descriptions. Loading projects from Google Sheets streamlined the process, allowing for easy management and organization.
-      
-      🔄 Dynamic Adjustments: Teaching Assistants in Action
-      
-      Teaching assistants took the reins with the power to adjust group sizes, introduce new projects, and reallocate students as needed. Flexibility became the key, ensuring the system adapted to the evolving needs of collaborative learning.
-      
-      🏆 From Hack-a-thon Triumph to Schoolwide Solution
-      
-      Lambda Group Organizer emerged victorious in a two-day hack-a-thon, addressing a critical need. Its success led to a school request for further development. Collaborating with a fellow alum, we transformed a winning idea into an indispensable tool for students and educators.
-      `,
-  },
-  {
-    images: [
-      {
         src: portfolio,
         alt: "Portfolio website styled to look like an apple desktop computer",
       },
     ],
     title: "My Portfolio",
-    shortDescription: `Embark on a visually captivating journey through my portfolio website, designed to echo the aesthetics of the Apple operating system. Here, innovation meets nostalgia, creating a unique experience that unfolds like a digital journey through familiar interfaces.`,
+    shortDescription: `Embark on a visually captivating journey through my portfolio website, designed to echo the aesthetics of the Apple operating system. Here, innovation meets nostalgia, creating a unique experience that unfolds...`,
     description: `Digital Odyssey: Jon Bernal's Portfolio Showcase
 
     Embark on a visually captivating journey through my portfolio website, designed to echo the aesthetics of the Apple operating system. Here, innovation meets nostalgia, creating a unique experience that unfolds like a digital journey through familiar interfaces.
@@ -87,5 +46,46 @@ export const sites = [
     Toggle to a Microsoft Windows '98 version of the site for a delightful throwback. Watch as a vintage CRT television warms up, casting nostalgic scan lines across the interface. Experience the charm of yesteryear with a modern twist.
     
     Join me on this interactive odyssey where past and present collide, showcasing not just websites but a narrative of creativity and innovation.`,
+  },
+  {
+    images: [
+      {
+        src: guild,
+        alt: "Guild Education Home Page",
+      },
+    ],
+    title: "Guild Education",
+    shortDescription: `Guild education is an enterprise level company. We worked with large companies like Target, Walmart, etc. I learned many lessons in what it takes to scale in employees...`,
+    description: `I built mainly backend services at guild.`,
+  },
+  {
+    images: [
+      {
+        src: lambda,
+        alt: "Lambda Group Organizer Home Page",
+      },
+    ],
+    title: "Lambda Group Organizer",
+    shortDescription: `While going to Lambda School, three of us signed up for a hackathon project. The project was so well received by the students and the staff, we were asked to make it production...`,
+    description: `Lambda Group Organizer: Streamlining Collaborations in Education
+
+      In the realm of collaborative projects, chaos reigned during group sign-ups at our school. Unintentional deletions, collisions, and tedious data formatting plagued the process. Enter Lambda Group Organizer, a solution born out of necessity and innovation.
+      
+      🌐 Addressing the Chaos: From Spreadsheet Woes to Seamless Sign-ups
+      
+      Say farewell to the spreadsheet struggles. Lambda Group Organizer revolutionized project sign-ups by empowering teachers and students alike. No more accidental deletions or clashes – this tool transformed the sign-up process into a seamless experience.
+      
+      📊 Teacher Empowerment: Effortless Project Management
+      
+      Teachers gained control with the ability to upload project details directly into the system, specifying student slots and project descriptions. Loading projects from Google Sheets streamlined the process, allowing for easy management and organization.
+      
+      🔄 Dynamic Adjustments: Teaching Assistants in Action
+      
+      Teaching assistants took the reins with the power to adjust group sizes, introduce new projects, and reallocate students as needed. Flexibility became the key, ensuring the system adapted to the evolving needs of collaborative learning.
+      
+      🏆 From Hack-a-thon Triumph to Schoolwide Solution
+      
+      Lambda Group Organizer emerged victorious in a two-day hack-a-thon, addressing a critical need. Its success led to a school request for further development. Collaborating with a fellow alum, we transformed a winning idea into an indispensable tool for students and educators.
+      `,
   },
 ];
