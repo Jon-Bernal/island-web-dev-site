9488e83cfca11fbc7113d86ef35769da53c2a337
jon Bernal <jbernalwrc@gmail.com>
2024-02-28 15:02:22 -0800

    WIP: add clsoe button to portfolio and html to site descriptions.

--- src/routes/portfolio/sites.js
+++ src/routes/portfolio/sites.js
@@ -13,8 +13,15 @@ export const sites = [
     ],
     title: "Nowigence",
     shortDescription: `This was the first startup I worked for. I learned a great many lessons while working with nowigence. The technology we used at the company completely metamorphasized in the two years I worked there...`,
-    description:
-      "This was the first startup I worked for. I learned a great many lessons while working with nowigence. The technology we used at the company completely metamorphasized in the two years I worked there.",
+    description: (
+      <>
+        <p>
+          This was the first startup I worked for. I learned a great many
+          lessons while working with nowigence. The technology we used at the
+          company completely metamorphasized in the two years I worked there.
+        </p>
+      </>
+    ),
   },
   {
     images: [
@@ -25,27 +32,57 @@ export const sites = [
     ],
     title: "My Portfolio",
     shortDescription: `Embark on a visually captivating journey through my portfolio website, designed to echo the aesthetics of the Apple operating system. Here, innovation meets nostalgia, creating a unique experience that unfolds...`,
-    description: `Digital Odyssey: Jon Bernal's Portfolio Showcase
+    description: (
+      <>
+        <h3>Digital Odyssey: Jon Bernal's Portfolio Showcase</h3>
+        <p>
+          Embark on a visually captivating journey through my portfolio website,
+          designed to echo the aesthetics of the Apple operating system. Here,
+          innovation meets nostalgia, creating a unique experience that unfolds
+          like a digital journey through familiar interfaces.
+        </p>
+        <h4>🖥️ Apple-Inspired Interface: Where Design Meets Functionality</h4>
 
-    Embark on a visually captivating journey through my portfolio website, designed to echo the aesthetics of the Apple operating system. Here, innovation meets nostalgia, creating a unique experience that unfolds like a digital journey through familiar interfaces.
-    
-    🖥️ Apple-Inspired Interface: Where Design Meets Functionality
-    
-    Immerse yourself in the familiarity of an Apple-esque operating system. Each 'app' serves as a portal to a different dimension of my digital world. Peek into the "About Me" window for a glimpse into the creator behind the code, or navigate to project sections to explore a curated list of websites I've crafted over time.
-    
-    🔧 Technological Tapestry: Unveiling the Code Behind
-    
-    Click through each project 'app' to uncover the intricate technologies woven into its fabric. From front-end frameworks to back-end wizardry, explore the diverse toolkit that powers these digital wonders.
-    
-    🌐 Social Media Hub: Connecting Beyond Code
-    
-    Navigate the system 'tray' to discover links to my relevant social media sites. Stay connected and follow the ongoing narrative of my digital endeavors.
-    
-    🕹️ Retro Reverie: Windows '98 Edition
-    
-    Toggle to a Microsoft Windows '98 version of the site for a delightful throwback. Watch as a vintage CRT television warms up, casting nostalgic scan lines across the interface. Experience the charm of yesteryear with a modern twist.
-    
-    Join me on this interactive odyssey where past and present collide, showcasing not just websites but a narrative of creativity and innovation.`,
+        <p>
+          Immerse yourself in the familiarity of an Apple-esque operating
+          system. Each 'app' serves as a portal to a different dimension of my
+          digital world. Peek into the "About Me" window for a glimpse into the
+          creator behind the code, or navigate to project sections to explore a
+          curated list of websites I've crafted over time.
+        </p>
+
+        <h4>🔧 Technological Tapestry: Unveiling the Code Behind</h4>
+
+        <p>
+          Click through each project 'app' to uncover the intricate technologies
+          woven into its fabric. From front-end frameworks to back-end wizardry,
+          explore the diverse toolkit that powers these digital wonders.
+        </p>
+
+        <h4>🌐 Social Media Hub: Connecting Beyond Code</h4>
+
+        <p>
+          Navigate the system 'tray' to discover links to my relevant social
+          media sites. Stay connected and follow the ongoing narrative of my
+          digital endeavors.
+        </p>
+
+        <h4>🕹️ Retro Reverie: Windows '98 Edition</h4>
+
+        <p>
+          Toggle to a Microsoft Windows '98 version of the site for a delightful
+          throwback. Watch as a vintage CRT television warms up, casting
+          nostalgic scan lines across the interface. Experience the charm of
+          yesteryear with a modern twist.
+        </p>
+
+        <p>
+          Join me on this interactive odyssey where past and present collide,
+          showcasing not just websites but a narrative of creativity and
+          innovation.
+        </p>
+      </>
+    ),
   },
   {
     images: [
@@ -56,7 +93,7 @@ export const sites = [
     ],
     title: "Guild Education",
     shortDescription: `Guild education is an enterprise level company. We worked with large companies like Target, Walmart, etc. I learned many lessons in what it takes to scale in employees...`,
-    description: `I built mainly backend services at guild.`,
+    description: <>I built mainly backend services at guild.</>,
   },
   {
     images: [
@@ -67,25 +104,50 @@ export const sites = [
     ],
     title: "Lambda Group Organizer",
     shortDescription: `While going to Lambda School, three of us signed up for a hackathon project. The project was so well received by the students and the staff, we were asked to make it production...`,
-    description: `Lambda Group Organizer: Streamlining Collaborations in Education
+    description: (
+      <>
+        <h3>
+          Lambda Group Organizer: Streamlining Collaborations in Education
+        </h3>
+        <p>
+          In the realm of collaborative projects, chaos reigned during group
+          sign-ups at our school. Unintentional deletions, collisions, and
+          tedious data formatting plagued the process. Enter Lambda Group
+          Organizer, a solution born out of necessity and innovation.
+        </p>
 
-      In the realm of collaborative projects, chaos reigned during group sign-ups at our school. Unintentional deletions, collisions, and tedious data formatting plagued the process. Enter Lambda Group Organizer, a solution born out of necessity and innovation.
-      
-      🌐 Addressing the Chaos: From Spreadsheet Woes to Seamless Sign-ups
-      
-      Say farewell to the spreadsheet struggles. Lambda Group Organizer revolutionized project sign-ups by empowering teachers and students alike. No more accidental deletions or clashes – this tool transformed the sign-up process into a seamless experience.
-      
-      📊 Teacher Empowerment: Effortless Project Management
-      
-      Teachers gained control with the ability to upload project details directly into the system, specifying student slots and project descriptions. Loading projects from Google Sheets streamlined the process, allowing for easy management and organization.
-      
-      🔄 Dynamic Adjustments: Teaching Assistants in Action
-      
-      Teaching assistants took the reins with the power to adjust group sizes, introduce new projects, and reallocate students as needed. Flexibility became the key, ensuring the system adapted to the evolving needs of collaborative learning.
-      
-      🏆 From Hack-a-thon Triumph to Schoolwide Solution
-      
-      Lambda Group Organizer emerged victorious in a two-day hack-a-thon, addressing a critical need. Its success led to a school request for further development. Collaborating with a fellow alum, we transformed a winning idea into an indispensable tool for students and educators.
-      `,
+        <h4>
+          🌐 Addressing the Chaos: From Spreadsheet Woes to Seamless Sign-ups
+        </h4>
+        <p>
+          Say farewell to the spreadsheet struggles. Lambda Group Organizer
+          revolutionized project sign-ups by empowering teachers and students
+          alike. No more accidental deletions or clashes – this tool transformed
+          the sign-up process into a seamless experience.
+        </p>
+        <h4>📊 Teacher Empowerment: Effortless Project Management</h4>
+        <p>
+          Teachers gained control with the ability to upload project details
+          directly into the system, specifying student slots and project
+          descriptions. Loading projects from Google Sheets streamlined the
+          process, allowing for easy management and organization.
+        </p>
+        <h4>🔄 Dynamic Adjustments: Teaching Assistants in Action</h4>
+        <p>
+          Teaching assistants took the reins with the power to adjust group
+          sizes, introduce new projects, and reallocate students as needed.
+          Flexibility became the key, ensuring the system adapted to the
+          evolving needs of collaborative learning.
+        </p>
+        <h4>🏆 From Hack-a-thon Triumph to Schoolwide Solution</h4>
+
+        <p>
+          Lambda Group Organizer emerged victorious in a two-day hack-a-thon,
+          addressing a critical need. Its success led to a school request for
+          further development. Collaborating with a fellow alum, we transformed
+          a winning idea into an indispensable tool for students and educators.
+        </p>
+      </>
+    ),
   },
 ];
