8b7d313438c34828d281c213599d3d576839c4a3
jon Bernal <jbernalwrc@gmail.com>
2024-03-01 13:49:45 -0800

    WIP: add my photo and intro text to home page.

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -4,13 +4,28 @@ import { Image, Carousel } from "grommet";
 
 import pluaris from "../../assets/pluaris.png";
 import portfolio1 from "../../assets/portfolio-1.png";
-import Guild from "../../assets/guild-1.png";
+import guild from "../../assets/guild-1.png";
+import headshot from "../../assets/headshot.png";
 
 export default function Home() {
   return (
     <div>
-      <p>Hello!</p>
-      <p>Welcome to Whidbey Island Web Dev!</p>
+      <section className={styles.aboveTheFold}>
+        <div className={styles.aboveTheFold_text}>
+          <p>
+            Hello! I'm Jon Bernal and welcome to Whidbey Island Web Development!
+            I'm a freelance web developer based right here in Clinton, WA. I
+            help local businesses and not for profits create, change or fix up
+            their websites and web applications
+          </p>
+        </div>
+        <img
+          src={headshot}
+          alt={"Pencil drawing of Jon Bernal"}
+          className={styles.aboveTheFold_image}
+        />
+      </section>
+
       <p>
         This site is still under construction, so please bear with me on things
         not being as polished as they could be. If you'd like to see a more
@@ -22,7 +37,7 @@ export default function Home() {
           <Carousel play={6000}>
             <Image src={pluaris} width={"100%"} />
             <Image src={portfolio1} width={"100%"} />
-            <Image src={Guild} width={"100%"} />
+            <Image src={guild} width={"100%"} />
           </Carousel>
         </div>
       </p>
